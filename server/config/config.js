439cce8013898aa8a2f14cff25e061ef4cf06706
Mario Fernandez Carmona <mariofernandezcarmona@MacBook-Pro-de-Mario-Fernandez.local>
2019-01-08 23:30:47 -0300

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,25 @@
 // =======================
 // Puerto
 // =======================
-process.env.PORT = process.env.PORT || 3001;
+process.env.PORT = process.env.PORT || 3000;
+
+
+// =======================
+// Entorno
+// =======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======================
+// BD Mongo
+// =======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev')
+    urlDB = 'mongodb://localhost:27017/cafe';
+else 
+    urlDB = 'mongodb://cafe-user:123456a@ds051848.mlab.com:51848/cafe';
+
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config.js');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 var bodyParser = require('body-parser');
 
 
@@ -12,38 +14,13 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.get('/usuario', function(req, res){
-    res.send('get Usuario');
-});
-
-app.post('/usuario', function(req, res){
-    let body = req.body;
-
-    if (body.nombre === undefined){
-        res.status(400).json({
-            ok : false,
-            mensaje : 'El nombre es necesario'
-        });
-    }else{
-        res.json({
-            body
-        });
-    }
+app.use(require('./routes/usuarios'))
 
 
-});
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true}, (err,res) => {
+    if (err) throw err;
 
-app.put('/usuario/:id', function(req, res){
-
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res){
-    res.send('delete Usuario');
+    console.log('Base de datos ONLINE')
 });
 
 app.listen(process.env.PORT, () => {
